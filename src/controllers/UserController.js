396073a1ca66025feba6f3e702e58ece539f06af
Wiktoria <wiktoriajuliasalamon@gmail.com>
2020-11-27 14:12:12 +0100

    feat(status-codes): add register and login error responses

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -8,7 +8,11 @@ const user = {
     userLogin: (req, res, next) => {
         passport.authenticate('local', function(err, user, info) {
         if (err) { return next(err); }
-        if (!user) { return res.sendStatus(403); }
+        if (!user) {
+            return res
+                .status(403)
+                .send("Invalid username or password");
+        }
 
         req.logIn(user, function(err) {
             if (err) { return next(err); }
@@ -22,16 +26,26 @@ const user = {
         res.sendStatus(200);
     },
     userRegister: (req, res) => {
-        console.log(req.body.username, req.body.password);
-        User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
-        if (err) {
-            console.log(err);
-            return res.sendStatus(500);
-        } else {
-            passport.authenticate('local')(req, res, function() {
-            res.sendStatus(201);
-            });
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res
+                .status(400)
+                .send("Username or password was not given");
         }
+        console.log(username, password);
+        User.register(new User({username}), password, function(err, user) {
+            if (err.name === 'UserExistsError') {
+                return res
+                    .status(409)
+                    .send("A user with the given username is already registered");
+            }
+            if (err) {
+                console.log(err);
+                return res.sendStatus(500);
+            }
+            passport.authenticate('local')(req, res, function() {
+                res.sendStatus(201);
+            });
         });
     },
 };
